7940d7ccb662c7196e887297fb8f73bf97bed6ba
zy <1191028469@qq.com>
2021-03-14 23:38:46 +0800

    cloud-config

---
+++ cloud-config-client-3355/src/main/java/com/lean/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.lean.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: ArronZY
+ * @Date: 2021/3/14 23:17
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/lean/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.lean.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ArronZY
+ * @Date: 2021/3/14 23:18
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
